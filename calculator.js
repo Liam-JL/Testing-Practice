f3b8e9c735caef6de02d704d5eb0b89c6a7380f0
LiamJL <paledeepened@gmail.com>
2025-05-28 12:19:55 +0100

    Implement add function in calculator object

---
+++ calculator.js
@@ -0,0 +1,14 @@
+export const calculator = {
+
+    add : function(a, b){
+        if (!a || !b) {
+            throw new Error(`Expected 2 numbers as arguments but received ${Number(!!a) + Number(!!b)}`)
+        }
+
+        if (typeof(a) !== "number" || typeof(b) !== "number") {
+            throw new TypeError(`Expected numbers but got ${typeof a} and ${typeof b}`);
+        }
+
+        return (a * 10 + b * 10) / 10;
+    },
+}
