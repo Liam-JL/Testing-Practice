a74d2b21c66c11a1965ac31dfaca5dc7041cdcb1
LiamJL <paledeepened@gmail.com>
2025-05-28 13:37:19 +0100

    Implement subtract function of calculator

--- calculator.js
+++ calculator.js
@@ -1,6 +1,6 @@
 export const calculator = {
 
-    add : function(a, b){
+    add : function(a, b) {
         if (!a || !b) {
             throw new Error(`Expected 2 numbers as arguments but received ${Number(!!a) + Number(!!b)}`)
         }
@@ -11,4 +11,16 @@ export const calculator = {
 
         return (a * 10 + b * 10) / 10;
     },
+
+    subtract : function(a, b) {
+        if (!a || !b) {
+            throw new Error(`Expected 2 numbers as arguments but received ${Number(!!a) + Number(!!b)}`)
+        }
+
+        if (typeof(a) !== "number" || typeof(b) !== "number") {
+            throw new TypeError(`Expected numbers but got ${typeof a} and ${typeof b}`);
+        }
+
+        return (a * 10 - b * 10) / 10;
+    }
 }
