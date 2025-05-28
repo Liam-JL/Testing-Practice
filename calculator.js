d27e2c90f0d11db8ef77f932fb8d6eb464c46990
LiamJL <paledeepened@gmail.com>
2025-05-28 14:46:01 +0100

    Implement multiply function in calculator

--- calculator.js
+++ calculator.js
@@ -22,5 +22,17 @@ export const calculator = {
         }
 
         return (a * 10 - b * 10) / 10;
+    },
+
+    multiply : function(a, b) {
+        if (!a || !b) {
+            throw new Error(`Expected 2 numbers as arguments but received ${Number(!!a) + Number(!!b)}`)
+        }
+
+        if (typeof(a) !== "number" || typeof(b) !== "number") {
+            throw new TypeError(`Expected numbers but got ${typeof a} and ${typeof b}`);
+        }
+
+        return (a * b * 10) / 10;
     }
 }
