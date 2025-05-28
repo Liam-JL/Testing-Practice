035b7d49f3bf8f4914dcdba0a4917f8c4c00fb64
LiamJL <paledeepened@gmail.com>
2025-05-28 16:28:24 +0100

    Implement helper function to validate amount of args given in calc functions

--- calculator.js
+++ calculator.js
@@ -1,4 +1,9 @@
 export const calculator = {
+    enforceArgsAmount : function(args) {
+        if (args.length !== 2) {
+            throw new Error(`Expected 2 numbers as arguments but received ${args.length}`)
+        }
+    },
 
     add : function(a, b) {
         if (!a || !b) {
@@ -24,10 +29,10 @@ export const calculator = {
         return (a * 10 - b * 10) / 10;
     },
 
-    multiply : function(a, b) {
-        if (!a || !b) {
-            throw new Error(`Expected 2 numbers as arguments but received ${Number(!!a) + Number(!!b)}`)
-        }
+    multiply : function(...args) {
+        this.enforceArgsAmount(args);
+
+        const [a, b] = args;
 
         if (typeof(a) !== "number" || typeof(b) !== "number") {
             throw new TypeError(`Expected numbers but got ${typeof a} and ${typeof b}`);
